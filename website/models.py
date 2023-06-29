a947b369bfbaaccdd7e3f9d6c0cd0f3ee4bdc8bb
Rokkam saikumar <137883882+Rsaikumaar@users.noreply.github.com>
2023-06-29 15:34:55 +0530

    Update models.py

--- website/models.py
+++ website/models.py
@@ -307,8 +307,7 @@ class Mail():
 			listener.stop()
 		listener = sr.Recognizer()
 		command = 'node'
-		device_index = 0 
-		with sr.Microphone(device_index=device_index) as source:
+		with sr.Microphone(device_index=None) as source:
 			print('Listening...')
 			voice = listener.listen(source, timeout=10, phrase_time_limit=5)    
 			try:
